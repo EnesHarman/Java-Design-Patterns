2fbdbda22c5c316dbf0fc76a51bbc9e4b57ee047
EnesHarman <enesharman60@gmail.com>
2022-07-06 12:36:12 +0300

    Add Adapter Pattern

---
+++ src/main/java/eh/java/Adapter/ExternalDistCalculatorImpl.java
@@ -0,0 +1,8 @@
+package eh.java.Adapter;
+
+public class ExternalDistCalculatorImpl implements ExternalDistanceCalculator{
+    @Override
+    public long calculate(String dest1, String dest2) {
+        return 1000;
+    }
+}
